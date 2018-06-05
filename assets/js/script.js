fa7660b1c353147c5448e0724dbba700b6755363
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-06-05 00:15:48 -0700

    fixing some stuff on the texts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var description1 = "Web Developer, passionate about technology, cooking and good movies.";
 var description2 = "I always wanted to work with technology. Right after I started College, I started my career as a Web Developer.";
-var description3 = "After a total five years at the company, I decided to have an international experience living in the United States for two years.";
+var description3 = "After a total five years at a website developing company called Netzee Digital Agency, I decided to have an international experience living in the United States for two years.";
 var description4 = "On my last 3 months, I decided to take the Coding Boot Camp at UC Berkeley, one of the best Universities in the country. It's been great and I'm living the best moment in my life.";
 
 var count = 0;
